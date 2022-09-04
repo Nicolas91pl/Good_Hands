d9a5ae97fd78e24b21824e4db01346e8a7d3143a
Patryk Miścior <nicolas91pl@o2.pl>
2022-09-04 19:42:02 +0200

    Model-Donation

--- goodhands/samaritan/models.py
+++ goodhands/samaritan/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     id = models.BigAutoField()
@@ -19,3 +20,15 @@ class Institution(models.Model):
     description = models.CharField(max_length=255)
     type = models.CharField(max_length=15, choices=TYPE_CHOICES, default=fund)
     categories = models.ManyToManyField(Category)
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.PROTECT)
+    address = models.CharField(max_length=120)
+    city = models.CharField(max_length=120)
+    zip_code = models.CharField(max_length=10)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.CharField(max_length=120)
+    user = models.ForeignKey(User, blank=True, default=None)
