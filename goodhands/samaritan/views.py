9f78f15b83360c3b3dabf4a8d85017050c9e33b2
Patryk Miścior <nicolas91pl@o2.pl>
2022-09-04 16:55:59 +0200

    Base views render

--- goodhands/samaritan/views.py
+++ goodhands/samaritan/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class LandingPage(View):
+    def get(self, request):
+        return render(request, "index.html")
+
+class AddDonation(View):
+    def get(self, request):
+        return render(request, "form.html")
+
+class Login(View):
+    def get(self, request):
+        return render(request, "login.html")
+
+class Register(View):
+    def get(self, request):
+        return render(request, "register.html")
