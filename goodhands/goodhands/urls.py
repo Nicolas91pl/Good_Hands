9c663ae4754d97612edf6fc16f4917a5f2366f05
Patryk Miścior <nicolas91pl@o2.pl>
2022-09-04 12:51:12 +0200

    refactor

--- goodhands/goodhands/urls.py
+++ goodhands/goodhands/urls.py
@@ -1,4 +1,4 @@
-"""Good_Hands URL Configuration
+"""goodhands URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
--- goodhands/samaritan/apps.py
+++ goodhands/samaritan/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SamaritanConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Samaritan'
+    name = 'samaritan'
