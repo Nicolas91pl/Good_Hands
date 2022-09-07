b6311859c3757c4636896da2ff4e910b38b9aa65
Patryk Miścior <nicolas91pl@o2.pl>
2022-09-07 21:07:33 +0200

    Dinamic institution loading

--- goodhands/samaritan/views.py
+++ goodhands/samaritan/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+from samaritan.models import Donation, Institution, Category
 
 class LandingPage(View):
     def get(self, request):
-        return render(request, "index.html")
+        fundations = list(Institution.objects.filter(type='fund'))
+        non_gov_orgs = list(Institution.objects.filter(type='non_gov_org'))
+        local_gathering = list(Institution.objects.filter(type='local'))
+        ctx = {
+            'fundations': fundations,
+            'non_gov_orgs': non_gov_orgs,
+            'local_gathering': local_gathering
+        }
+        return render(request, "index.html", ctx)
 
 class AddDonation(View):
     def get(self, request):
