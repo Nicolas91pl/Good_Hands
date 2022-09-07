1fd3d80639ed955db19108b35be5454a03e55c4c
Patryk Miścior <nicolas91pl@o2.pl>
2022-09-07 20:00:47 +0200

    All quantity sum and all organizations, dinamic display

--- goodhands/samaritan/views.py
+++ goodhands/samaritan/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.views import View
+from samaritan.models import Donation, Institution
 
 class LandingPage(View):
     def get(self, request):
-        return render(request, "index.html")
+        donations = list(Donation.objects.all())
+        organizations = list(Institution.objects.all())
+        quantity_sum = 0
+        for don in donations:
+            quantity_sum = quantity_sum + int(don.quantity)
+
+        organization_sum = len(organizations)
+
+        ctx = {
+            "quantity_sum": quantity_sum,
+            "organization_sum": organization_sum
+        }
+        return render(request, "index.html", ctx)
 
 class AddDonation(View):
     def get(self, request):
