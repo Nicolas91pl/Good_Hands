757ae2b9ddef012c51cb51f17b59356f0eb4180b
Patryk Miścior <nicolas91pl@o2.pl>
2022-09-07 21:36:51 +0200

    Working register

--- goodhands/samaritan/views.py
+++ goodhands/samaritan/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.models import User
 
 class LandingPage(View):
     def get(self, request):
@@ -16,3 +17,14 @@ class Login(View):
 class Register(View):
     def get(self, request):
         return render(request, "register.html")
+
+    def post(self, request):
+        name = request.POST.get("name")
+        surname = request.POST.get("surname")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        password2 = request.POST.get("password2")
+
+        if password == password2:
+            User.objects.create(username=email, first_name=name, last_name=surname, email=email, password=password)
+            return redirect(f'/login')
